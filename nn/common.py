c100e9921a214c3040e2d5974c98df67617cb1de
Ryan Davis <epikit@gmail.com>
2023-02-02 08:09:53 -0600

    'Economy' forecasting stack now uses NacMlp as default fn for everything

--- nn/common.py
+++ nn/common.py
@@ -12,6 +12,11 @@ from torch.autograd import Variable
 import torch.nn.functional as F
 from typing import *
 
+from nn.core import *
+
+from itertools import chain
+from cytoolz import *
+
 class EconomyModule(Module):
    def __init__(self, n_symbols:int) -> None:
       super().__init__()
@@ -35,8 +40,12 @@ class MarketImageEncoder(MarketModule):
       self.n_input_terms = n_input_terms
       self.n_output_terms = n_output_terms
       
-      self.term_encoder = Linear(n_input_terms, 1)
-      self.seq_encoder = Linear(n_steps, n_output_terms)
+      self.term_encoder = autofn(n_input_terms, 1)
+      self.seq_encoder = autofn(n_steps, n_output_terms)
+      
+   def parameters(self, recurse: bool = True) -> Iterator[Parameter]:
+      # return super().parameters(recurse)
+      return chain(self.term_encoder.parameters(), self.seq_encoder.parameters())
       
    def forward(self, market_image:Tensor):
       seq = Variable(zeros((self.n_steps,)))
@@ -80,9 +89,9 @@ class MarketImageDecoder(Module):
       self.n_output_steps = n_output_steps
       self.n_output_terms = n_output_terms
       
-      self.step_decoder = Linear(n_input_terms, n_output_steps)
-      self.term_decoder = Linear(1+n_input_terms+n_output_steps, n_output_terms)
-      self.term_revisor = Linear(1+n_output_terms, n_output_terms)
+      self.step_decoder = autofn(n_input_terms, n_output_steps)
+      self.term_decoder = autofn(1+n_input_terms+n_output_steps, n_output_terms)
+      self.term_revisor = autofn(1+n_output_terms, n_output_terms)
       self.output_shape = output_shape
       
    def forward(self, X:Tensor):
@@ -100,7 +109,18 @@ class MarketImageDecoder(Module):
          
       return output_image
    
+   def parameters(self, recurse: bool = True) -> Iterator[Parameter]:
+      # return super().parameters(recurse)
+      return chain(self.term_decoder.parameters(), self.step_decoder.parameters(), self.term_revisor.parameters())
+   
 class EconomyForecastingModule(EconomyModule):
+   market_encoder:Union[Callable[[Tensor], Tensor], List[Callable[[Tensor], Tensor]]]
+   market_decoder:Union[Callable[[Tensor], Tensor], List[Callable[[Tensor], Tensor]]]
+   
+   forecast_shape:Tuple[int]
+   n_symbols:int
+   shared_transform:bool
+   
    def __init__(self, n_symbols:int=0, forecast_shape=None, market_transform=(None, None), shared_transform=True):
       super().__init__(n_symbols)
       assert n_symbols is not None and n_symbols > 0
@@ -120,11 +140,34 @@ class EconomyForecastingModule(EconomyModule):
          self.market_encoder = [clone_module(market_encoder) for _ in range(n_symbols)]
          self.market_decoder = [clone_module(market_decoder) for _ in range(n_symbols)]
          
+   def parameters(self, recurse: bool = True) -> Iterator[Parameter]:
+      if self.shared_transform:
+         assert isinstance(self.market_encoder, Module)
+         assert isinstance(self.market_decoder, Module)
+         return chain(self.market_encoder.parameters(recurse=recurse), self.market_decoder.parameters(recurse=recurse))
+      else:
+         assert isinstance(self.market_encoder, list)
+         assert isinstance(self.market_decoder, list)
+         return chain(
+            *(l.parameters() for l in self.market_encoder),
+            *(l.parameters() for l in self.market_decoder)
+         )
+         
    def encode_market(self, symbol:int, market:Tensor):
-      return (self.market_encoder if self.shared_transform else self.market_encoder[symbol])(market)
+      if callable(self.market_encoder):
+         return self.market_encoder(market)
+      
+      else:
+         return self.market_encoder[symbol](market)
+      # return (self.market_encoder if self.shared_transform else self.market_encoder[symbol])(market)
    
    def decode_market(self, symbol:int, terms:Tensor):
-      return (self.market_decoder if self.shared_transform else self.market_decoder[symbol])(terms)
+      # return (self.market_decoder if self.shared_transform else self.market_decoder[symbol])(terms)
+      if callable(self.market_decoder):
+         return self.market_decoder(terms)
+      
+      else:
+         return self.market_decoder[symbol](terms)
 
    def forward(self, markets:Tensor):
       forecast = Variable(zeros((self.n_symbols, *self.forecast_shape)))
@@ -136,109 +179,9 @@ class EconomyForecastingModule(EconomyModule):
          # print(y)
          forecast[symbol] = y
       return forecast
-      
-def torch_vectorize(f, inplace=False, end_dim=None):
-   #TODO: implement this such that it can be jitted by pytorch
-   def wrapper(tensor:Tensor, **params) -> Tensor:
-      x = tensor if inplace else tensor.clone()
-      xshape = x.shape
-      
-      view = x.flatten() if end_dim is None else x.flatten(end_dim=end_dim)
 
-      y = torch.reshape(f(view, **params), xshape)
+from nn.namlp import NacMlp
 
-      return y
-
-   return wrapper
-
-def clone_module(module, memo=None):
-    """
-    [[Source]](https://github.com/learnables/learn2learn/blob/master/learn2learn/utils.py)
-    **Description**
-    Creates a copy of a module, whose parameters/buffers/submodules
-    are created using PyTorch's torch.clone().
-    This implies that the computational graph is kept, and you can compute
-    the derivatives of the new modules' parameters w.r.t the original
-    parameters.
-    **Arguments**
-    * **module** (Module) - Module to be cloned.
-    **Return**
-    * (Module) - The cloned module.
-    **Example**
-    ~~~python
-    net = nn.Sequential(Linear(20, 10), nn.ReLU(), nn.Linear(10, 2))
-    clone = clone_module(net)
-    error = loss(clone(X), y)
-    error.backward()  # Gradients are back-propagate all the way to net.
-    ~~~
-    """
-    # NOTE: This function might break in future versions of PyTorch.
-
-    # TODO: This function might require that module.forward()
-    #       was called in order to work properly, if forward() instanciates
-    #       new variables.
-    # TODO: We can probably get away with a shallowcopy.
-    #       However, since shallow copy does not recurse, we need to write a
-    #       recursive version of shallow copy.
-    # NOTE: This can probably be implemented more cleanly with
-    #       clone = recursive_shallow_copy(model)
-    #       clone._apply(lambda t: t.clone())
-
-    if memo is None:
-        # Maps original data_ptr to the cloned tensor.
-        # Useful when a Module uses parameters from another Module; see:
-        # https://github.com/learnables/learn2learn/issues/174
-        memo = {}
-
-    # First, create a copy of the module.
-    # Adapted from:
-    # https://github.com/pytorch/pytorch/blob/65bad41cbec096aa767b3752843eddebf845726f/torch/nn/modules/module.py#L1171
-    if not isinstance(module, torch.nn.Module):
-        return module
-    clone = module.__new__(type(module))
-    clone.__dict__ = module.__dict__.copy()
-    clone._parameters = clone._parameters.copy()
-    clone._buffers = clone._buffers.copy()
-    clone._modules = clone._modules.copy()
-
-    # Second, re-write all parameters
-    if hasattr(clone, '_parameters'):
-        for param_key in module._parameters:
-            if module._parameters[param_key] is not None:
-                param = module._parameters[param_key]
-                param_ptr = param.data_ptr
-                if param_ptr in memo:
-                    clone._parameters[param_key] = memo[param_ptr]
-                else:
-                    cloned = param.clone()
-                    clone._parameters[param_key] = cloned
-                    memo[param_ptr] = cloned
-
-    # Third, handle the buffers if necessary
-    if hasattr(clone, '_buffers'):
-        for buffer_key in module._buffers:
-            if clone._buffers[buffer_key] is not None and \
-                    clone._buffers[buffer_key].requires_grad:
-                buff = module._buffers[buffer_key]
-                buff_ptr = buff.data_ptr
-                if buff_ptr in memo:
-                    clone._buffers[buffer_key] = memo[buff_ptr]
-                else:
-                    cloned = buff.clone()
-                    clone._buffers[buffer_key] = cloned
-                    memo[param_ptr] = cloned
-
-    # Then, recurse for each submodule
-    if hasattr(clone, '_modules'):
-        for module_key in clone._modules:
-            clone._modules[module_key] = clone_module(
-                module._modules[module_key],
-                memo=memo,
-            )
-
-    # Finally, rebuild the flattened parameters for RNNs
-    # See this issue for more details:
-    # https://github.com/learnables/learn2learn/issues/139
-    if hasattr(clone, 'flatten_parameters'):
-        clone = clone._apply(lambda x: x)
-    return clone
+def autofn(nin:int, nout:int, hidden=None):
+   # hidden = [nout, nout] if hidden is None else hidden
+   return NacMlp(nin, nout, hidden)
